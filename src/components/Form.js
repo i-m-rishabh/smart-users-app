0a078931393cfb17ed5d88640c16ba83f8c58b3e
i-m-rishabh <uniquepandey125@gmail.com>
2023-06-08 23:25:55 +0530

    added scrollbar styling and fixes some bugs

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,19 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import './Form.css';
 import Warning from './Warning.js';
 import Backdrop from './Backdrop.js';
 
 const Form = (props) => {
     const [isValid, setIsValid] = useState(true);
-    const [name, setName] = useState('');
-    const [age, setAge] = useState('');
+    // const [name, setName] = useState('');
+    // const [age, setAge] = useState('');
     const [errorMessage, setErrorMessage] = useState('')
+    const nameRef = useRef(null);
+    const ageRef = useRef(null);
 
     function formSubmitHandler(event) {
+        let name = nameRef.current.value;
+        let age = ageRef.current.value;
         event.preventDefault();
         if (name.trim().length === 0 || age.trim().length === 0) {
             setErrorMessage('Please enter all the details');
@@ -20,19 +24,21 @@ const Form = (props) => {
                 setIsValid(false);
             } else {
                 props.OnAddUser(name, parseInt(age));
-                setName('');
-                setAge('');
+                // setName('');
+                // setAge('');
+                nameRef.current.value = '';
+                ageRef.current.value = '';
             }
         }
     }
 
-    function handleNameChange(event) {
-        isValid && setName(event.target.value);
-    }
+    // function handleNameChange(event) {
+    //     isValid && setName(event.target.value);
+    // }
 
-    function handleAgeChange(event) {
-        setAge(event.target.value);
-    }
+    // function handleAgeChange(event) {
+    //     setAge(event.target.value);
+    // }
 
     function handleConfirm() {
         setIsValid(true);
@@ -41,9 +47,9 @@ const Form = (props) => {
         <div className='container'>
             <form onSubmit={formSubmitHandler}>
                 <label htmlFor="user_name">Username</label>
-                <input type="text" name="user_name" id="user_name" onChange={handleNameChange} value={name} />
+                <input type="text" name="user_name" id="user_name" placeholder='enter name' ref={nameRef} />
                 <label htmlFor="user_age">Age (Years)</label>
-                <input type="number" name="user_age" id="user_age" onChange={handleAgeChange} value={age} />
+                <input type="number" name="user_age" id="user_age" placeholder='enter age(years)' ref={ageRef} />
                 <button type="submit">Add User</button>
             </form>
             {
